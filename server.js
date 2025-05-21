366c7c29206f71c8b8130572eefed85c9a3e3cb7
Rishabh_Garai <rishabhgarai33@gmail.com>
2025-05-21 19:08:35 +0530

    4th

--- server.js
+++ server.js
@@ -239,6 +239,6 @@ app.get('/', (req, res) => {
   }
 });
 
-app.listen(3000, () => {
-  console.log('✅ Server running at http://localhost:3000');
+app.listen(3000, '0.0.0.0', () => {
+  console.log(`✅ Server running`);
 });
